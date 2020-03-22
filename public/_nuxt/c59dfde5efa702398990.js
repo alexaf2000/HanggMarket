ede5f4b01703f738369d3337a654051bb27a9c0f
Alex A.F <alex2andres@gmail.com>
2020-03-22 01:57:45 +0100

    Updates rendered frontend

---
+++ public/_nuxt/c59dfde5efa702398990.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{211:function(e,n,t){"use strict";t.r(n);var l={head:function(){return{title:"Mi perfil"}}},r=t(12),component=Object(r.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
