f563f6848e336c78d91eae5d91bd081d988a76ce
alexaf2000 <alex2andres@gmail.com>
2020-03-24 01:09:42 +0100

    axios on error 500 don't throw nothing

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,57 +1,45 @@
-import axios from 'axios'
+import axios from "axios";
+import Vuesax from "vuesax";
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 export default ({ app, store, redirect }) => {
-    axios.defaults.baseURL = process.env.apiUrl
+    axios.defaults.baseURL = process.env.apiUrl;
 
     if (process.server) {
-        return
+        return;
     }
 
     // Request interceptor
-    axios.interceptors.request.use((request) => {
-        request.baseURL = process.env.apiUrl
+    axios.interceptors.request.use(request => {
+        request.baseURL = process.env.apiUrl;
 
-        const token = store.getters['auth/token']
+        const token = store.getters["auth/token"];
 
         if (token) {
-            request.headers.common.Authorization = `Bearer ${token}`
+            request.headers.common.Authorization = `Bearer ${token}`;
         }
 
-        return request
-    })
+        return request;
+    });
 
     // Response interceptor
-    axios.interceptors.response.use(response => response, (error) => {
-        const { status } = error.response || {}
+    axios.interceptors.response.use(
+        response => response,
+        error => {
+            const { status } = error.response || {};
 
-        if (status >= 500) {
-            swal({
-                type: 'error',
-                title: app.i18n.t('error_alert_title'),
-                text: app.i18n.t('error_alert_text'),
-                reverseButtons: true,
-                confirmButtonText: app.i18n.t('ok'),
-                cancelButtonText: app.i18n.t('cancel')
-            })
+            if (status >= 500) {
+                console.log("Error 500");
+            }
+
+            if (status === 401 && store.getters["auth/check"]) {
+                store.commit("auth/LOGOUT");
+
+                redirect({ name: "login" });
+            }
+
+            return Promise.reject(error);
         }
-
-        if (status === 401 && store.getters['auth/check']) {
-            swal({
-                type: 'warning',
-                title: app.i18n.t('token_expired_alert_title'),
-                text: app.i18n.t('token_expired_alert_text'),
-                reverseButtons: true,
-                confirmButtonText: app.i18n.t('ok'),
-                cancelButtonText: app.i18n.t('cancel')
-            }).then(() => {
-                store.commit('auth/LOGOUT')
-
-                redirect({ name: 'login' })
-            })
-        }
-
-        return Promise.reject(error)
-    })
-}
+    );
+};
