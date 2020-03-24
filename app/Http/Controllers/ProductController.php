65b9c287f68443ba0f0a067b50d2796d4f84fd51
Alex A.F <alex2andres@gmail.com>
2020-03-24 18:54:31 +0100

    Created Product controller, unless update method

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductRequest;
+use App\Products\Product;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    /**
+     * Display a listing Products with images, actual price and categories.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Set the day as today
+        $date = Carbon::today();
+        // Return the Product with images, categories and JUST the price of today
+        // ? Not used the Price model function because is not compatible with paginate
+        return Product::with(['Images', 'Categories', 'Prices' => function ($q) use ($date) {
+            $q->whereDate('date_start', '<=', $date);
+            $q->whereDate('date_end', '>=', $date);
+            $q->first();
+        }])->paginate(15);
+    }
+
+    /**
+     * Store a newly created Product in storage.
+     * @requires name,description,barcode,categories,price,date_end
+     * @allow not use image, and date_start(from price)
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProductRequest $request)
+    {
+
+        // Add own attributes
+        $Product = new Product(['name' => $request->name, 'description' => $request->description, 'barcode' => $request->barcode]);
+
+        // Saves to database
+        $Product->save();
+
+        // Adds categories
+        $Product->categories()->attach($request->categories);
+
+
+        // if start date does not exists
+        if (!isset($request->date_start)) {
+            // Use today
+            $request->date_start = Carbon::today();
+        }
+
+        // Creates this product price
+        $Product->Prices()->create([
+            'value' => $request->price,
+            'date_start' => $request->date_start,
+            'date_end' => Carbon::parse($request->date_end)
+        ]);
+
+        if (isset($request->images)) {
+            foreach ($request->images as $image) {
+                $Product->Images()->create(["image" => $image]);
+            }
+        }
+
+        return $Product;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        // Generates the date as today
+        $date = Carbon::today();
+
+        // Returns the product with images, categories and price of today
+        $product = Product::with(['Images', 'Categories', 'Prices' => function ($q) use ($date) {
+            $q->whereDate('date_start', '<=', $date);
+            $q->whereDate('date_end', '>=', $date);
+            $q->first();
+        }])->find($product->id);
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProductRequest $request, Product $product)
+    {
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->forceDelete;
+    }
+}
