bb82bda1585ea26048f72996fbba79fa33433342
alexaf2000 <alex2andres@gmail.com>
2020-03-25 19:32:52 +0100

    Requires authentication on all routes (unless login)

--- routes/api.php
+++ routes/api.php
@@ -17,33 +17,38 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', 'AuthController@login');
 
-// Products routes
-Route::group(['prefix' => 'product'], function () {
-    Route::get('/', 'ProductController@index');
-    Route::post('/store', 'ProductController@store');
-    Route::get('/show/{product}/prices', 'ProductController@showPrices');
-    Route::get('/show/{product}', 'ProductController@show');
-    Route::post('/update/{product}', 'ProductController@update');
-    Route::delete('/destroy/{product}', 'ProductController@destroy');
-});
-// Category routes
-Route::group(['prefix' => 'category', 'middleware' => 'auth:api'], function () {
-    Route::get('/', 'CategoryController@index');
-    Route::post('/store', 'CategoryController@store');
-    Route::get('/show/{category}', 'CategoryController@show');
-    Route::post('/update/{category}', 'CategoryController@update');
-    Route::delete('/destroy/{category}', 'CategoryController@destroy');
-});
+// Required login routes
 
-// Users routes
-Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function () {
-    Route::get('/', 'UserController@index');
-    Route::post('/store', 'UserController@store');
-    Route::get('/show/{user}', 'UserController@show');
-    Route::post('/update/{user}', 'UserController@update');
-    Route::delete('/destroy/{user}', 'UserController@destroy');
-});
+Route::group(['middleware' => 'auth:api'], function () {
 
-Route::middleware('auth:api')->get('/details', function (Request $request) {
-    return $request->user();
+    // Products routes
+    Route::group(['prefix' => 'product'], function () {
+        Route::get('/', 'ProductController@index');
+        Route::post('/store', 'ProductController@store');
+        Route::get('/show/{product}/prices', 'ProductController@showPrices');
+        Route::get('/show/{product}', 'ProductController@show');
+        Route::post('/update/{product}', 'ProductController@update');
+        Route::delete('/destroy/{product}', 'ProductController@destroy');
+    });
+    // Category routes
+    Route::group(['prefix' => 'category', 'middleware' => 'auth:api'], function () {
+        Route::get('/', 'CategoryController@index');
+        Route::post('/store', 'CategoryController@store');
+        Route::get('/show/{category}', 'CategoryController@show');
+        Route::post('/update/{category}', 'CategoryController@update');
+        Route::delete('/destroy/{category}', 'CategoryController@destroy');
+    });
+
+    // Users routes
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/', 'UserController@index');
+        Route::post('/store', 'UserController@store');
+        Route::get('/show/{user}', 'UserController@show');
+        Route::post('/update/{user}', 'UserController@update');
+        Route::delete('/destroy/{user}', 'UserController@destroy');
+    });
+
+    Route::get('/details', function (Request $request) {
+        return $request->user();
+    });
 });
